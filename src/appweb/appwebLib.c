f64d92bf3f0155f22a10d1b4415a87d1bf0564d8
Embedthis Software <dev@embedthis.com>
2010-08-28 18:14:49 -0700

    Update search for win 7 support

--- src/appweb/appwebLib.c
+++ src/appweb/appwebLib.c
@@ -7053,6 +7053,7 @@ static bool parseHeader(MaConn *conn, MprCmd *cmd)
     value = 0;
 
     buf = mprGetCmdBuf(cmd, MPR_CMD_STDOUT);
+    mprAddNullToBuf(buf);
     headers = mprGetBufStart(buf);
 
     /*
@@ -7066,8 +7067,7 @@ static bool parseHeader(MaConn *conn, MprCmd *cmd)
                 /* Not EOF and less than max headers and have not yet seen an end of headers delimiter */
                 return 0;
             }
-        } 
-        len = 2;
+        } else len = 2;
     } else {
         len = 4;
     }
