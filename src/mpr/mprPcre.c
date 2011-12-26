7fb7bf41d2dd70bffe7b0ac27d53267860d71cc8
Embedthis Software <dev@embedthis.com>
2011-12-26 11:05:40 -0800

    Update for new version

--- src/mpr/mprPcre.c
+++ src/mpr/mprPcre.c
@@ -57,6 +57,12 @@
 #define MPR_CPU_SH4         12
 
 
+#if VXWORKS
+    #ifndef _VSB_CONFIG_FILE
+        #define _VSB_CONFIG_FILE "vsbConfig.h"
+    #endif
+#endif
+
 #if BLD_UNIX_LIKE && !VXWORKS && !MACOSX && !FREEBSD
     #include    <sys/types.h>
     #include    <time.h>
@@ -147,7 +153,7 @@
     #include    <stdio.h>
     #include    <stdlib.h>
     #include    <string.h>
-    #include    <symSyncLib.h>
+    #include    <symSync.h>
     #include    <sysSymTbl.h>
     #include    <sys/fcntlcom.h>
     #include    <sys/ioctl.h>
@@ -551,8 +557,12 @@ extern "C" {
     #endif
     #define __WALL          0
 #else
-    #define O_BINARY        0
-    #define O_TEXT          0
+    #ifndef O_BINARY
+        #define O_BINARY    0
+    #endif
+    #ifndef O_TEXT
+        #define O_TEXT      0
+    #endif
 
     /*
      *  For some reason it is removed from fedora 6 pthreads.h and only comes in for UNIX96
@@ -585,8 +595,12 @@ extern "C" {
 
     #define MPR_BINARY      ""
     #define MPR_TEXT        ""
-    #define O_BINARY        0
-    #define O_TEXT          0
+    #ifndef O_BINARY
+        #define O_BINARY    0
+    #endif
+    #ifndef O_TEXT
+        #define O_TEXT      0
+    #endif
     #define SOCKET_ERROR    -1
     #define MSG_NOSIGNAL    0
     #define __WALL          0
@@ -1043,6 +1057,16 @@ extern int gettimeofday(struct timeval *tv, struct timezone *tz);
 #endif
 
 typedef int64 MprOff;
+
+/*
+    Socklen_t
+ */
+#if VXWORKS
+    typedef int MprSocklen;
+#else
+    typedef socklen_t MprSocklen;
+#endif
+
 
 /*
     Backward compatibility aliases
@@ -8564,6 +8588,12 @@ functions whose names all begin with "_pcre_". */
 
 #ifndef PCRE_INTERNAL_H
 #define PCRE_INTERNAL_H
+
+#if VXWORKS
+    #ifndef _VSB_CONFIG_FILE
+        #define _VSB_CONFIG_FILE "vsbConfig.h"
+    #endif
+#endif
 
 #include "buildConfig.h"
 
--- src/mpr/mprSsl.c
+++ src/mpr/mprSsl.c
@@ -56,6 +56,12 @@
 #define MPR_CPU_SH4         12
 
 
+#if VXWORKS
+    #ifndef _VSB_CONFIG_FILE
+        #define _VSB_CONFIG_FILE "vsbConfig.h"
+    #endif
+#endif
+
 #if BLD_UNIX_LIKE && !VXWORKS && !MACOSX && !FREEBSD
     #include    <sys/types.h>
     #include    <time.h>
@@ -146,7 +152,7 @@
     #include    <stdio.h>
     #include    <stdlib.h>
     #include    <string.h>
-    #include    <symSyncLib.h>
+    #include    <symSync.h>
     #include    <sysSymTbl.h>
     #include    <sys/fcntlcom.h>
     #include    <sys/ioctl.h>
@@ -550,8 +556,12 @@ extern "C" {
     #endif
     #define __WALL          0
 #else
-    #define O_BINARY        0
-    #define O_TEXT          0
+    #ifndef O_BINARY
+        #define O_BINARY    0
+    #endif
+    #ifndef O_TEXT
+        #define O_TEXT      0
+    #endif
 
     /*
      *  For some reason it is removed from fedora 6 pthreads.h and only comes in for UNIX96
@@ -584,8 +594,12 @@ extern "C" {
 
     #define MPR_BINARY      ""
     #define MPR_TEXT        ""
-    #define O_BINARY        0
-    #define O_TEXT          0
+    #ifndef O_BINARY
+        #define O_BINARY    0
+    #endif
+    #ifndef O_TEXT
+        #define O_TEXT      0
+    #endif
     #define SOCKET_ERROR    -1
     #define MSG_NOSIGNAL    0
     #define __WALL          0
@@ -1042,6 +1056,16 @@ extern int gettimeofday(struct timeval *tv, struct timezone *tz);
 #endif
 
 typedef int64 MprOff;
+
+/*
+    Socklen_t
+ */
+#if VXWORKS
+    typedef int MprSocklen;
+#else
+    typedef socklen_t MprSocklen;
+#endif
+
 
 /*
     Backward compatibility aliases
